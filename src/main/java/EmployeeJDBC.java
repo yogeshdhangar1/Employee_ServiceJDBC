02a129cd95c19734467ee12f0189d207d54af158
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-13 08:42:58 -0800

    Added: Ability To Add New Emo To Savepoint

--- src/main/java/EmployeeJDBC.java
+++ src/main/java/EmployeeJDBC.java
@@ -1,20 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
 public class EmployeeJDBC {
     public static void main(String[] args) throws SQLException {
         Connection con = null;
+       Savepoint savepoint1 = null;
+       Savepoint savepoint2 = null;
+       Savepoint savepoint3 = null;
+
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","mysql@1234");
             con.setAutoCommit(false);
             Statement stm = con.createStatement();
             //stm.execute("select * from payroll_details");
             stm.execute("insert into payroll_details values(1,'Amol',73667373,'Khokrale','Farmer','male',20000,2000,1000,200,18000,2020-10-22,'Nandurbar','IN');");
+            savepoint1 = con.setSavepoint();
             stm.execute("insert into emp values (11,'Yogesh','Dhangar','Abc@123);");
+            savepoint2 = con.setSavepoint();
+            stm.executeUpdate("update emp set lname = 'borse' where id = 11); ");
+            savepoint3 = con.setSavepoint();
         } catch (SQLException e) {
             e.printStackTrace();
-            con.rollback();
+            con.rollback(savepoint1);
         }finally {
             con.commit();
         }
